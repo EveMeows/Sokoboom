257dfcc7c24e5a4ad27a5802c82025f42595ed65
Evelyn Serra <143884194+EveMeows@users.noreply.github.com>
2025-03-05 21:53:11 +0200

    Add project files.

---
+++ Sokoboom/Sokoban.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Sokoboom;
+
+public class Sokoban : Game
+{
+    private GraphicsDeviceManager graphics;
+    private SpriteBatch spriteBatch = null!;
+
+    public Sokoban()
+    {
+        this.graphics = new GraphicsDeviceManager(this);
+        this.Content.RootDirectory = "Content";
+        this.IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        this.spriteBatch = new SpriteBatch(this.GraphicsDevice);
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        this.GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        base.Draw(gameTime);
+    }
+}
---
+++ Sokoboom/Startup.cs
@@ -0,0 +1,2 @@
+﻿using var game = new Sokoboom.Sokoban();
+game.Run();
